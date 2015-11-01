273be4da45fb0b94b552a36ae38c1e820376d6b0
ChengYi He <chengyihetaipei@gmail.com>
2015-11-02 05:21:11 +0800

    fix child process stuck in zombie state due to parent process
    If a child process exits, its state will become
    zombie and waits for its parent process to reap
    it. However, if its parent process doesn't reap
    it, then the state of this child process will stay
    in zombie state.
    
    Fix this symptom by letting the parent process
    waidpid to reap its child process.
    
    Log of running this program:
    $ adb shell /data/child-process-zombie-due-to-parent
    7918:7918, program is starting
    7918:7918, parent process after fork
    7918:7918, parent process waits for child process to terminate
    7920:7918, child process after fork
    7920:7918, child process is ready to _exit(2)
    7918:7918, child pid 7920 exited with status 2
    7918:7918, parent process is ready to exit

--- ChildProcessZombieDueToParent.cpp
+++ ChildProcessZombieDueToParent.cpp
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main()
 {
-    pid_t pid;
+    pid_t pid, wpid;
+    int status;
 
     printf("%d:%d, program is starting\n", getpid(), gettid());
 
     pid = fork();
     if (pid == 0) {
         printf("%d:%d, child process after fork\n", getpid(), gettid());
+        usleep(5 * 1000000);
         printf("%d:%d, child process is ready to _exit(2)\n", getpid(), gettid());
         _exit(2);
     }
 
     printf("%d:%d, parent process after fork\n", getpid(), gettid());
-    while (1) {
-        //printf("%d:%d, parent process is looping\n", getpid(), gettid());
-        usleep(1000000);
+    printf("%d:%d, parent process waits for child process to terminate\n", getpid(), gettid());
+
+    wpid  = waitpid(pid, &status, 0);
+    if (WIFEXITED(status)) {
+        printf("%d:%d, child pid %d exited with status %d\n", getpid(), gettid(), wpid, WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+        printf("%d:%d, child pid %d killed by signal %d\n", getpid(), gettid(), wpid, WTERMSIG(status));
+    } else if (WIFSTOPPED(status)) {
+        printf("%d:%d, child pid %d stopped by signal %d\n", getpid(), gettid(), wpid, WSTOPSIG(status));
+    } else {
+        printf("%d:%d, child pid %d state changed\n", getpid(), gettid(), wpid);
     }
+
+    printf("%d:%d, parent process is ready to exit\n", getpid(), gettid());
 
     return 0;
 }
